a694905ef3c5c51991e1f3cd1350ee2fd78ecf75
farah <farah.fournat@gmail.com>
2021-10-14 23:36:43 +0200

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2021,11,31,11,30,0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+giveaway.textContent = `giveaway ends on ${month} ${year} ${hours}: ${minutes} am`;
